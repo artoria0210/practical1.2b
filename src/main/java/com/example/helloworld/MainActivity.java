1fd3903baf3ec06467c2d01bb79fbc10160a0b69
artoria0210 <66988774+artoria0210@users.noreply.github.com>
2020-07-04 01:08:59 +0800

    Commit by Chua Guangwen

--- src/main/java/com/example/helloworld/MainActivity.java
+++ src/main/java/com/example/helloworld/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,15 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button mResetButton;
+    private Button mCountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        mResetButton = findViewById(R.id.button_zero);
+        mCountButton = findViewById(R.id.button_count);
     }
-
-
 
 
     public void showToast(View view) {
@@ -34,6 +38,31 @@ public class MainActivity extends AppCompatActivity {
         if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
         }
+        mResetButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        mResetButton.setClickable(true);
+
+        switch (mCount % 2) {
+//            if mCount mod 2 is 0, number is even otherwise odd
+            case 0:
+                view.setBackgroundColor(Color.MAGENTA);
+                break;
+            case 1:
+                view.setBackgroundColor(Color.GREEN);
+                break;
+            default:
+                break;
+        }
     }
-}
+
+        public void resetZero (View view){
+            mCount = 0;
+            mShowCount.setText(String.valueOf(mCount));
+
+            view.setClickable(false);
+            view.setBackgroundColor(Color.GRAY);
+            mCountButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        }
+    }
+
+
 
